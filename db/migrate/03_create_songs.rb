cce74ac26cf40816b18a217e702289fb70140452
Mickey <micksheridan.24@gmail.com>
2019-03-03 21:43:22 -0500

    submit

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,12 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def up
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
+
+  def down
+  end
 end
